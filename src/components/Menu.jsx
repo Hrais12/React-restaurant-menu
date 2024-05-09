8de472509e0c1ba3cb7b95f20e089c39975e69a3
Houda Rais <houdarais@Houdas-Air.fios-router.home>
2024-05-08 23:13:18 -0400

    category menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,61 @@
 import React from "react";
 import Category from "./Category";
+import { useState } from "react";
 
-function Menu({ menu, action, total }) {
+function Menu({ menu, action }) {
+  const [showCategory, setShowCategory] = useState({
+    mainCourse: true,
+    appetizer: false,
+    dessert: false,
+  });
+  const visibile = (category) => {
+    setShowCategory({
+      ...showCategory,
+      [category]: !showCategory[category], //replace with true
+    });
+  };
+
   return (
     <div className="menu">
       <h1>Menu</h1>
 
-      {Object.keys(menu).map((category) => (
-        <Category
-          key={category}
-          menuItems={menu[category]}
-          addAction={action}
-          category={category}
-          totl={total}
-        />
-      ))}
+      <nav>
+        <ul className="menuCategory">
+          <li>
+            <button
+              className="categoryBtn"
+              onClick={() => visibile("mainCourse")}
+            >
+              Main Course
+            </button>
+          </li>
+          <li>
+            <button
+              className="categoryBtn"
+              onClick={() => visibile("appetizer")}
+            >
+              Appetizer
+            </button>
+          </li>
+          <li>
+            <button className="categoryBtn" onClick={() => visibile("dessert")}>
+              Dessert
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      {Object.keys(menu).map(
+        (category) =>
+          showCategory[category] && (
+            <Category
+              key={category}
+              menuItems={menu[category]}
+              addAction={action}
+              category={category}
+            />
+          )
+      )}
     </div>
   );
 }
